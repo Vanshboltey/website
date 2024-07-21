0272ae927e1376e90fe514ffb98d7500188616df
Vanshboltey <158207690+Vanshboltey@users.noreply.github.com>
2024-07-21 23:47:39 +0530

    Update setwebhook.php

--- setwebhook.php
+++ setwebhook.php
@@ -1,6 +1,6 @@
 <?php
 $config = include('config.php');
-$botToken = $config['6786898794:AAGUoU3FxGMxOIaJ8zJ5kDPd3svT42HarF8'];
+$botToken = $config['bot_token'];
 $webhookUrl = 'https://vanshnetwork.co/bot.php';
 
 $response = file_get_contents("https://api.telegram.org/bot{$botToken}/setWebhook?url={$webhookUrl}");
