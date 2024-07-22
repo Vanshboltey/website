4da735b9c8fb19df4cf51c1d016d2c61693c52aa
Vanshboltey <158207690+Vanshboltey@users.noreply.github.com>
2024-07-22 08:50:55 +0530

    Delete setwebhook.php

--- setwebhook.php
+++
@@ -1,8 +0,0 @@
-<?php
-$config = include('config.php');
-$botToken = $config['bot_token'];
-$webhookUrl = 'https://vanshnetwork.co/bot.php';
-
-$response = file_get_contents("https://api.telegram.org/bot{$botToken}/setWebhook?url={$webhookUrl}");
-
-echo $response;
